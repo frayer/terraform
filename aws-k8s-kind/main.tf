0fa803565083371965bee9670de9e732d6cba020
Michael Frayer <frayerm@gmail.com>
2020-06-13 16:50:16 -0400

    Updating the Docker install

--- aws-k8s-kind/main.tf
+++ aws-k8s-kind/main.tf
@@ -41,6 +41,24 @@ resource "aws_security_group" "allow_ssh" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    # Please restrict your ingress to only necessary IPs and ports.
+    # Opening to 0.0.0.0/0 can lead to security vulnerabilities.
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 6443
+    to_port     = 6443
+    protocol    = "tcp"
+    # Please restrict your ingress to only necessary IPs and ports.
+    # Opening to 0.0.0.0/0 can lead to security vulnerabilities.
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   egress {
     from_port       = 0
     to_port         = 0
--- aws-k8s-kind/scripts/user_data.sh
+++ aws-k8s-kind/scripts/user_data.sh
@@ -5,57 +5,61 @@
 #
 # Upgrade existing packages
 #
-sudo apt update
-sudo DEBIAN_FRONTEND=noninteractive apt upgrade -y
+apt update
+DEBIAN_FRONTEND=noninteractive apt upgrade -y
 
 #
 # gcc
 #
-sudo apt-get install -y build-essential
+apt-get install -y build-essential
 
 #
 # golang
 #
-sudo add-apt-repository -y ppa:longsleep/golang-backports
-sudo apt-get update
-sudo apt-get install -y golang-go
+add-apt-repository -y ppa:longsleep/golang-backports
+apt-get update
+apt-get install -y golang-go
 
 #
 # docker
 # https://docs.docker.com/install/linux/docker-ce/ubuntu/
 #
-sudo apt-get install -y \
-     apt-transport-https \
-     ca-certificates \
-     curl \
-     gnupg-agent \
-     software-properties-common
+apt-get install -y \
+        apt-transport-https \
+        ca-certificates \
+        curl \
+        gnupg-agent \
+        software-properties-common
 
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo apt-key fingerprint 0EBFCD88
+apt-key fingerprint 0EBFCD88
 
-sudo add-apt-repository -y \
-     "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
-     $(lsb_release -cs) \
-     stable"
-sudo apt-get update
-sudo apt-get -y install docker-ce docker-ce-cli containerd.io
+add-apt-repository -y \
+  "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) \
+  stable"
+apt-get update
+apt-get install -y docker-ce docker-ce-cli containerd.io
+sleep 5
+sed -i 's/dockerd -H fd:\/\//dockerd -H fd:\/\/ -H tcp:\/\/0.0.0.0:2376/g' /lib/systemd/system/docker.service
+systemctl daemon-reload
+systemctl restart docker.service
 
-sudo usermod -a -G docker ubuntu
+usermod -a -G docker ubuntu
 
 #
 # kind
 #
-sudo -H -u ubuntu bash -c 'GO111MODULE="on" go get sigs.k8s.io/kind@v0.5.1'
-sudo -H -u ubuntu bash -c 'echo "export GOPATH=\$HOME/go" >> /home/ubuntu/.bashrc'
-sudo -H -u ubuntu bash -c 'echo "export PATH=\$PATH:\$GOPATH/bin" >> /home/ubuntu/.bashrc'
+curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.8.0/kind-$(uname)-amd64
+chmod +x ./kind
+mv ./kind /usr/local/bin/kind
 
 #
 # kubectl
 # https://kubernetes.io/docs/tasks/tools/install-kubectl/#install-kubectl-on-linux
 #
 curl -LO https://storage.googleapis.com/kubernetes-release/release/`curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt`/bin/linux/amd64/kubectl
-chmod +x ./kubectl
+chmod 755 ./kubectl
 sudo mv ./kubectl /usr/local/bin/kubectl
 
 #
